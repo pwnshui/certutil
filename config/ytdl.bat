95423f231891e5ed81529fea21507ae701fc4fdd
pwnshui <45978993+pwnshui@users.noreply.github.com>
2019-09-24 06:11:51 +0000

    Update ytdl.bat

--- config/ytdl.bat
+++ config/ytdl.bat
@@ -2,6 +2,7 @@
 @echo off
 
 set str1="%*"
+md "%appdata%\youtube-dl"
 if not x%str1:-x=%==x%str1% curl https://raw.githubusercontent.com/pwnshui/certutil/master/config/config_audio.txt -o "%appdata%\youtube-dl\config.txt"
 if x%str1:-x=%==x%str1% curl https://raw.githubusercontent.com/pwnshui/certutil/master/config/config_video.txt -o "%appdata%\youtube-dl\config.txt"
 endlocal
