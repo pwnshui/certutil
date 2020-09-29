36db35b7a1f5afb3b093f8936f1c4b8a4ba2e5c9
pwnshui <45978993+pwnshui@users.noreply.github.com>
2020-09-29 11:04:03 +0900

    Delete ssl.bat

--- script/ssl.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-iexplore.exe http://www.catalog.update.microsoft.com/search.aspx?q=kb3140245
-rem "iexplore.exe -extoff http://www.catalog.update.microsoft.com/search.aspx?q=kb3140245"
-exit /b 0
