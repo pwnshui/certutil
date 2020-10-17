8cdef279a0d107399de7a1d557c99f039927c01c
pwnshui <45978993+pwnshui@users.noreply.github.com>
2020-10-17 19:18:02 +0900

    Create hyu.bat

---
+++ windows/bin/truebin/hyu.bat
@@ -0,0 +1,9 @@
+taskkill /F /IM SearchUI.exe
+move "%windir%\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy" "%windir%\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy.bak"
+
+rem Running this script can cause various issues with Explorer, WSL, etc.
+rem See https://github.com/W4RH4WK/Debloat-Windows-10/issues/250
+
+taskkill /F /IM ShellExperienceHost.exe
+move "%windir%\SystemApps\ShellExperienceHost_cw5n1h2txyewy" "%windir%\SystemApps\ShellExperienceHost_cw5n1h2txyewy.bak"
+
